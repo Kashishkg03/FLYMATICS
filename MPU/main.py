4aa779b9fec6d40d38102d4466aabc7d758b0e82
Kashishkg03 <97089600+Kashishkg03@users.noreply.github.com>
2023-04-29 19:51:17 +0530

    Update main.py

--- MPU/main.py
+++ MPU/main.py
@@ -2,7 +2,7 @@ from machine import Pin, I2C
 import utime
 from mpu6050 import init_mpu6050, get_mpu6050_data
  
-i2c = I2C(0, scl=Pin(21), sda=Pin(20), freq=400000)
+i2c = I2C(1, scl=Pin(3), sda=Pin(2), freq=400000)
 init_mpu6050(i2c)
  
 while True:
@@ -10,4 +10,4 @@ while True:
     print("Temperature: {:.2f} °C".format(data['temp']))
     print("Acceleration: X: {:.2f}, Y: {:.2f}, Z: {:.2f} g".format(data['accel']['x'], data['accel']['y'], data['accel']['z']))
     print("Gyroscope: X: {:.2f}, Y: {:.2f}, Z: {:.2f} °/s".format(data['gyro']['x'],data['gyro']['y'], data['gyro']['z']))
-    utime.sleep(0.5)
+    utime.sleep(5)
